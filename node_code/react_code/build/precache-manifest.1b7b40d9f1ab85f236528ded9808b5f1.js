0eecf1a6bc0716e64c2054bea59a235cc387a1bb
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-03-05 00:04:49 -0700

    first commit

---
+++ node_code/react_code/build/precache-manifest.1b7b40d9f1ab85f236528ded9808b5f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da026695a436465463a9b1cf901de4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9659d63895b5c965e5",
+    "url": "/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "d7cbc00020db6cab167d",
+    "url": "/static/css/main.bb0ce7da.chunk.css"
+  },
+  {
+    "revision": "bf9659d63895b5c965e5",
+    "url": "/static/js/2.2900138a.chunk.js"
+  },
+  {
+    "revision": "d7cbc00020db6cab167d",
+    "url": "/static/js/main.629769c8.chunk.js"
+  },
+  {
+    "revision": "7c74a28aacb750ece18c",
+    "url": "/static/js/runtime-main.c36b00b5.js"
+  },
+  {
+    "revision": "7eb07ee14818757d325ecfd1dc6ab45f",
+    "url": "/static/media/00_default_image.7eb07ee1.jpg"
+  },
+  {
+    "revision": "7e476855d680a810ebbdc28e6eaa96ef",
+    "url": "/static/media/GC.7e476855.jpg"
+  },
+  {
+    "revision": "d2bafbb3e87ce721223ec74dad8c4066",
+    "url": "/static/media/back.d2bafbb3.jpg"
+  },
+  {
+    "revision": "d6514b8914154b95e8d12fbafd9d9701",
+    "url": "/static/media/ball33.d6514b89.png"
+  },
+  {
+    "revision": "b2a42e56ee447d5846ab747ed71ffa8d",
+    "url": "/static/media/ghora3.b2a42e56.jpg"
+  }
+]);
